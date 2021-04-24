7ffac0f228a43f43cbf5694da1d44d3132310ad4
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2021-04-23 20:53:29 -0500

    Update client.py

--- src/osu/client.py
+++ src/osu/client.py
@@ -270,6 +270,9 @@ class Client:
         }
 
     def create_new_pm(self, target_id, message, is_action):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'target_id': target_id, 'message': message, 'is_action': is_action}
         resp = self.http.post(self, Path.create_new_pm(), data=data)
         return {
@@ -279,6 +282,9 @@ class Client:
         }
 
     def get_updates(self, since, channel_id=None, limit=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         resp = self.http.post(self, Path.get_updates(), since=since, channel_id=channel_id, limit=limit)
         return {
             'presence': [ChatChannel(channel) for channel in resp['presence']],
@@ -286,29 +292,53 @@ class Client:
         }
 
     def get_channel_messages(self, channel_id, limit=None, since=None, until=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [ChatMessage(msg) for msg in self.http.post(self, Path.get_channel_messages(channel_id), limit=limit, since=since, until=until)]
 
     def send_message_to_channel(self, channel_id, message, is_action):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'message': message, 'is_action': is_action}
         return ChatMessage(self.http.post(self, Path.send_message_to_channel(channel_id), data=data))
 
     def join_channel(self, channel, user):
+        """
+        *Will write documentation for this function soon...*
+        """
         return ChatChannel(self.http.put(self, Path.join_channel(channel, user)))
 
     def leave_channel(self, channel, user):
+        """
+        *Will write documentation for this function soon...*
+        """
         self.http.delete(self, Path.leave_channel(channel, user))
 
     def mark_channel_as_read(self, channel, message, channel_id, message_id):
+        """
+        *Will write documentation for this function soon...*
+        """
         self.http.put(self, Path.mark_channel_as_read(channel, message), channel_id=channel_id, message_id=message_id)
 
     def get_channel_list(self):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [ChatChannel(channel) for channel in self.http.get(self, Path.get_channel_list())]
 
     def create_channel(self, type, target_id=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'type': type, 'target_id': target_id}
         return ChatChannel(self.http.post(self, Path.create_channel(), data=data))
 
     def get_channel(self, channel):
+        """
+        *Will write documentation for this function soon...*
+        """
         resp = self.http.get(self, Path.get_channel(channel))
         return {
             'channel': ChatChannel(resp['channel']),
@@ -316,33 +346,60 @@ class Client:
         }
 
     def get_comments(self, commentable_type=None, commentable_id=None, cursor=None, parent_id=None, sort=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.get(self, Path.get_comments(), commentable_type=commentable_type, commentable_id=commentable_id,
                                            cursor=cursor, parent_id=parent_id, sort=sort))
 
     def post_comment(self, commentable_id=None, commentable_type=None, message=None, parent_id=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.post(self, Path.post_new_comment(), commentable_id=commentable_id, commentable_type=commentable_type,
                                             message=message, parent_id=parent_id))
 
     def get_comment(self, comment):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.get(self, Path.get_comment(comment)))
 
     def edit_comment(self, comment, message=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.patch(self, Path.edit_comment(comment), message=message))
 
     def delete_comment(self, comment):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.delete(self, Path.delete_comment(comment)))
 
     def add_comment_vote(self, comment):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.post(self, Path.add_comment_vote(comment)))
 
     def remove_comment_vote(self, comment):
+        """
+        *Will write documentation for this function soon...*
+        """
         return CommentBundle(self.http.delete(self, Path.remove_comment_vote(comment)))
 
     def reply_topic(self, topic, body):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'body': body}
         return ForumPost(self.http.post(self, Path.reply_topic(topic), data=data))
 
     def create_topic(self, body, forum_id, title, with_poll=None, hide_results=None, length_days=None, max_options=None, poll_options=None, poll_title=None, vote_change=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'body': body, 'forum_id': forum_id, 'title': title, 'with_poll': with_poll}
         if with_poll:
             if poll_options is None or poll_title is None:
@@ -359,6 +416,9 @@ class Client:
         }
 
     def get_topic_and_posts(self, topic, cursor=None, sort=None, limit=None, start=None, end=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         resp = self.http.get(self, Path.get_topic_and_posts(topic), cursor=cursor, sort=sort, limit=limit, start=start, end=end)
         return {
             'cursor': Cursor(resp['cursor']),
@@ -368,14 +428,23 @@ class Client:
         }
 
     def edit_topic(self, topic, topic_title):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'forum_topic': {'topic_title': topic_title}}
         return ForumTopic(self.http.patch(self, Path.edit_topic(topic), data=data))
 
     def edit_post(self, post, body):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'body': body}
         return ForumPost(self.http.patch(self, Path.edit_post(post), data=data))
 
     def search(self, mode=None, query=None, page=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         resp = self.http.get(self, Path.search(), mode=mode, query=query, page=page)
         return {
             'user': {'results': resp['user']['data'], 'total': resp['user']['total']} if mode is None or mode is 'all' or mode is 'user' else None,
@@ -383,18 +452,30 @@ class Client:
         }
 
     def get_user_highscore(self, room, playlist, user):
+        """
+        *Will write documentation for this function soon...*
+        """
         # Doesn't say response type
         return self.http.get(self, Path.get_user_high_score(room, playlist, user))
 
     def get_scores(self, room, playlist, limit=None, sort=None, cursor=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         # Doesn't say response type
         return self.http.get(self, Path.get_scores(room, playlist), limit=limit, sort=sort, cursor=cursor)
 
     def get_score(self, room, playlist, score):
-        # Doesn't sat response type
+        """
+        *Will write documentation for this function soon...*
+        """
+        # Doesn't say response type
         return self.http.get(self, Path.get_score(room, playlist, score))
 
     def get_notification(self, max_id=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         resp = self.http.get(self, Path.get_notifications(), max_id=max_id)
         return {
             'has_more': resp['has_more'],
@@ -404,40 +485,76 @@ class Client:
         }
 
     def mark_notifications_read(self, ids):
+        """
+        *Will write documentation for this function soon...*
+        """
         data = {'ids': ids}
         self.http.post(self, Path.mark_notifications_as_read(), data=data)
 
     def revoke_current_token(self):
+        """
+        *Will write documentation for this function soon...*
+        """
         self.http.delete(self, Path.revoke_current_token())
 
     def get_ranking(self, mode, type, country=None, cursor=None, filter=None, spotlight=None, variant=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return Rankings(self.http.get(self, Path.get_ranking(mode, type), country=country, cursor=cursor, filter=filter,
                                       spotlight=spotlight, variant=variant))
 
     def get_spotlights(self):
+        """
+        *Will write documentation for this function soon...*
+        """
         return Spotlights(self.http.get(self, Path.get_spotlights()))
 
     def get_own_data(self, mode):
+        """
+        *Will write documentation for this function soon...*
+        """
         return User(self.http.get(self, Path.get_own_data(mode)))
 
     def get_user_kudosu(self, user, limit=None, offset=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [KudosuHistory(kud) for kud in self.http.get(self, Path.get_user_kudosu(user), limit=limit, offset=offset)]
 
     def get_user_scores(self, user, type, include_fails=None, mode=None, limit=None, offset=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [Score(score) for score in self.http.get(self, Path.get_user_scores(user, type), include_fails=include_fails, mode=mode, limit=limit, offset=offset)]
 
     def get_user_beatmaps(self, user, type, limit=None, offset=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [Beatmapset(bm) for bm in self.http.get(self, Path.get_user_beatmaps(user, type), limit=limit, offset=offset)]
 
     def get_user_recent_activity(self, user, limit=None, offset=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [Event(event) for event in self.http.get(self, Path.get_user_recent_activity(user), limit=limit, offset=offset)]
 
     def get_user(self, user, mode='', key=None):
+        """
+        *Will write documentation for this function soon...*
+        """
         return User(self.http.get(self, Path.get_user(user, mode), key=key))
 
     def get_users(self, ids):
+        """
+        *Will write documentation for this function soon...*
+        """
         return [UserCompact(user) for user in self.http.get(self, Path.get_users(), ids=ids)]
 
     def get_wiki_page(self, locale, path, page):
+        """
+        *Will write documentation for this function soon...*
+        """
         return WikiPage(self.http.get(self, Path.get_wiki_page(locale, path), page=page))
 
