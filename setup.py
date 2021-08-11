0eef4746f1e8b474db8c1b5fadbc8bd0fd5e28da
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2021-08-11 17:46:26 -0400

    readme fix

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ import setuptools
 with open('requirements.txt', 'r') as f:
     requirements = f.readlines()
 
+readme = ''
+with open('README.rst') as f:
+    readme = f.read()
+
 setuptools.setup(install_requires=requirements,
+                 long_description=readme,
                  long_description_content_type="text/x-rst")
