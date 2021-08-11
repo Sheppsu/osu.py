c255cec7d566fb56f4b42151183433371806f44e
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2021-08-11 18:24:32 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import re
 
 with open('requirements.txt', 'r') as f:
     requirements = f.readlines()
@@ -7,6 +8,32 @@ readme = ''
 with open('README.rst') as f:
     readme = f.read()
 
-setuptools.setup(install_requires=requirements,
-                 long_description=readme,
-                 long_description_content_type="text/x-rst")
+version = ''
+with open('src/osu/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+project_urls = {
+    "Bug Tracker": "https://github.com/Sheepposu/osu.py/issues",
+    "osu.py documentation": "https://osupy.readthedocs.io/en/latest/",
+    "osu!api v2 documentation": "https://osu.ppy.sh/docs/index.html",
+    "Code examples": "https://github.com/Sheepposu/osu.py/tree/main/examples"
+}
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setuptools.setup(
+    name="osu.py",
+    version=version,
+    author="Sheepposu",
+    description="API Wrapper for osu!api v2 written in Python.",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    install_requires=requirements,
+    project_urls=project_urls,
+    classifiers=classifiers,
+    python_requires=">=3.6",
+    license="MIT"
+)
