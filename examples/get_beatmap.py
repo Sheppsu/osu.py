fd45759ef0c1a1ae74b8ece0f2d45b221b17ad21
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-05-31 17:14:32 -0400

    added get_beatmaps example

--- examples/get_beatmap.py
+++ examples/get_beatmap.py
@@ -8,6 +8,11 @@ redirect_url = "http://127.0.0.1:8080"
 
 client = Client.from_client_credentials(client_id, client_secret, redirect_url)
 
-ID = 1031991
-beatmap = client.get_beatmap(ID)
-print(beatmap.beatmapset.title)
+bm_id = 1031991
+beatmap = client.get_beatmap(bm_id)
+print(f"{beatmap.beatmapset.artist} - {beatmap.beatmapset.title}")
+
+bm_ids = [1765081, 1454407, 2987037, 3356285, 1530633, 1693575]
+beatmaps = client.get_beatmaps(bm_ids)
+for beatmap in beatmaps:
+    print(f"{beatmap.beatmapset.artist} - {beatmap.beatmapset.title}")
