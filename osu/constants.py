56ca52153251167e045cf67ef663454624eca7ef
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-06-03 19:30:51 -0400

    mod_abbreviations

--- osu/constants.py
+++ osu/constants.py
@@ -2,12 +2,29 @@ base_url = "https://osu.ppy.sh/api/v2/"
 auth_url = "https://osu.ppy.sh/oauth/authorize/"
 token_url = "https://osu.ppy.sh/oauth/token/"
 
-int_to_status = {
-    -2: 'graveyard',
-    -1: 'wip',
-    0: 'pending',
-    1: 'ranked',
-    2: 'approved',
-    3: 'qualified',
-    4: 'loved',
+mod_abbreviations = {
+    "NF": "NoFail",
+    "EZ": "Easy",
+    "TD": "TouchDevice",
+    "HD": "Hidden",
+    "HR": "HardRock",
+    "SD": "SuddenDeath",
+    "DT": "DoubleTime",
+    "RL": "Relax",
+    "HT": "HalfTime",
+    "NC": "Nightcore",
+    "FL": "Flashlight",
+    "AT": "Autoplay",
+    "SO": "SpunOut",
+    "AP": "Relax2",
+    "PF": "Perfect",
+    "FI": "FadeIn",
+    "RD": "Random",
+    "CM": "LastMod",
+    "TP": "TargetPractice",
+    "CP": "Coop",
+    "SV2": "ScoreV2",
+    "MR": "Mirror",
+    "KM": "keyMod",  # Actually not sure about this one
+    **{f"{k}K": f"Key{k}" for k in range(1, 10)}
 }
