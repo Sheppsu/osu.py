467eaeee912b42d5da2de5b729f33dcd809a5182
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-06-03 19:31:16 -0400

    import enums

--- osu/__init__.py
+++ osu/__init__.py
@@ -4,6 +4,7 @@ from .client import Client
 from .exceptions import *
 from .notification import NotificationWebsocket
 from .asyncio.client import AsynchronousClient
+from .enums import *
 
 
 __version__ = '0.2.7'
