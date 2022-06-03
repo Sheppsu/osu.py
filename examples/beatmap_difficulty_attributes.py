f59ded03a95df9a9bcf88acab69f78d8b9ea57af
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-06-03 19:31:04 -0400

    add use of Mods enum

--- examples/beatmap_difficulty_attributes.py
+++ examples/beatmap_difficulty_attributes.py
@@ -1,4 +1,4 @@
-from osu import Client
+from osu import Client, Mods
 import os
 
 
@@ -9,7 +9,11 @@ redirect_url = "http://127.0.0.1:8080"
 client = Client.from_client_credentials(client_id, client_secret, redirect_url)
 
 bm_id = 3149839
-attributes = client.get_beatmap_attributes(bm_id, ruleset="osu")
+HDHR = Mods.get_from_abbreviation("HD") | Mods.HardRock  # This is a valid argument for mods
+# HDHR = Mods.get_from_list([Mods.Hidden, Mods.get_from_abbreviation("HR")])  # This is also way to construct the mods argument
+HDDTHRFL = [HDHR, "DT", "Flashlight"]  # This is also a valid argument for mods
+attributes = client.get_beatmap_attributes(bm_id, ruleset="osu", mods=HDDTHRFL)  # String names of the mods will automatically be parsed
+print(f"Star rating: {attributes.star_rating}")
 for attr in dir(attributes.mode_attributes):
     if not attr.startswith("__"):
         print(f"{attr}: {getattr(attributes.mode_attributes, attr)}")
