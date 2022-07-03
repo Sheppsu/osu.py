1405a444bdd6c5a17b7b5f8f11edfed7e6f8e4dd
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-07-03 15:18:51 -0400

    v0.2.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Sheepposu'
 author = 'Sheepposu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.8'
+release = '0.2.9'
 
 
 # -- General configuration ---------------------------------------------------
