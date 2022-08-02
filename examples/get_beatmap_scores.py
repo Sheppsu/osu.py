17f5547748cef98b32ec7cd3436b2ec3beec2d57
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-08-02 01:46:09 -0400

    print mods of score

--- examples/get_beatmap_scores.py
+++ examples/get_beatmap_scores.py
@@ -8,7 +8,8 @@ redirect_url = "http://127.0.0.1:8080"
 
 client = Client.from_client_credentials(client_id, client_secret, redirect_url)
 
-ID = 1031991
+ID = 509388
 beatmap_scores = client.get_beatmap_scores(ID)
 print("Top 5 scores on this beatmap:")
-print("\n".join([f'{i+1}. {score.user.username} - {score.pp}' for i, score in enumerate(beatmap_scores.scores[:5])]))
+print("\n".join([f'{i+1}. {score.user.username} {"+"+score.mods.to_readable_string() if score.mods is not None else ""} - '
+                 f'{score.pp}' for i, score in enumerate(beatmap_scores.scores[:5])]))
