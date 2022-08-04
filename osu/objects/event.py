714d5f840d1fa669244bbf0a69f2ac3272e0cc21
Ilya Sukhanenkov <isuhanen@gmail.com>
2022-08-04 15:33:31 +0300

    fix: check if previous username is present in EventUser data

--- osu/objects/event.py
+++ osu/objects/event.py
@@ -127,7 +127,7 @@ class EventUser:
     def __init__(self, data):
         self.username = data['username']
         self.url = data['url']
-        self.previous_username = data['previousUsername']
+        self.previous_username = data.get('previous_username', None)
 
     def __repr__(self):
         return prettify(self, 'username', 'url')
