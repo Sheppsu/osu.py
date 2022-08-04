4b612f268394dace26a0a655e4b9711d65d92bbe
Ilya Sukhanenkov <isuhanen@gmail.com>
2022-08-04 16:33:07 +0300

    fix: fixed credentials

--- pytest_tests/constants.py
+++ pytest_tests/constants.py
@@ -1,3 +1,5 @@
-CLIENT_ID =
-CLIENT_SECRET =
-REDIRECT_URI =
+import os
+
+CLIENT_ID = int(os.getenv("CLIENT_ID"))
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
