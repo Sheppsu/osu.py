69bbfb5d084c3aa70a9ce484b07cec60e850dc8d
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-08-06 08:16:34 -0400

    comply with file name change

--- osu/objects/__init__.py
+++ osu/objects/__init__.py
@@ -8,7 +8,7 @@ from .comment import *
 from .event import *
 from .forum import *
 from .group import *
-from .multiplayer_score import *
+from .multiplayer import *
 from .notification import *
 from .ranking import *
 from .user import *
