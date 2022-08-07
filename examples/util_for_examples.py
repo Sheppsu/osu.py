a78780007b1bcfe03f88c4692a9d09dca3860320
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-08-07 03:36:36 -0400

    util for getting auth client

---
+++ examples/util_for_examples.py
@@ -0,0 +1,30 @@
+import json
+import os
+from osu import Client, AuthHandler, Scope
+
+
+def get_user_client():
+    def save(auth):
+        with open("auth.json", "w") as f:
+            json.dump(auth.get_save_data(), f)
+
+    with open("auth.json", "r") as f:
+        auth_data = json.load(f)
+
+    if auth_data:
+        print("Loading auth from save data.")
+        client = Client(AuthHandler.from_save_data(auth_data))
+    else:
+        print("No auth data found. Creating auth from scratch.")
+        client_id = int(os.getenv('osu_client_id'))
+        client_secret = os.getenv('osu_client_secret')
+        redirect_url = "http://127.0.0.1:8080"
+
+        auth = AuthHandler(client_id, client_secret, redirect_url, Scope.identify())
+        print(auth.get_auth_url())
+        auth.get_auth_token(input("Code: "))
+        client = Client(auth)
+
+    save(client.auth)
+    client.auth.set_refresh_callback(save)  # auto save auth data
+    return client
