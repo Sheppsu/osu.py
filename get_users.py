6934224583d12934bdac73e69dec7f52f2e605a2
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-09-06 00:02:09 -0400

    new example

---
+++ get_users.py
@@ -0,0 +1,15 @@
+from osu import Client
+import os
+
+
+client_id = int(os.getenv('osu_client_id'))
+client_secret = os.getenv('osu_client_secret')
+redirect_url = "http://127.0.0.1:8080"
+
+client = Client.from_client_credentials(client_id, client_secret, redirect_url)
+
+user_ids = [7562902, 11443437, 3717598, 1473890, 4230827]
+mode = 'osu'
+users = client.get_users(user_ids)
+for user in users:
+    print(user.username)
