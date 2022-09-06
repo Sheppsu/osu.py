95afbd077559534c9b353eb2742417eb8cdcc256
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-09-05 23:27:34 -0400

    fix index error

--- examples/get_scores.py
+++ examples/get_scores.py
@@ -1,7 +1,10 @@
 from util_for_examples import get_user_client
+import random
 
 client = get_user_client()
 
-room = client.get_rooms()[49]
+rooms = client.get_rooms()
+print(rooms, len(rooms))
+room = random.choice(rooms)
 scores = client.get_scores(room.id, room.playlist[0].id)
 print(scores)
