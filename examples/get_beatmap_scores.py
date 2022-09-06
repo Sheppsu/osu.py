0fc878c3febcd1d9c2f7bf045ff0b765e3024ea2
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2022-09-05 23:27:48 -0400

    put lazer example in there

--- examples/get_beatmap_scores.py
+++ examples/get_beatmap_scores.py
@@ -1,4 +1,4 @@
-from osu import Client, GameModeStr
+from osu import Client, GameModeStr, Mods
 import os
 
 
@@ -8,14 +8,24 @@ redirect_url = "http://127.0.0.1:8080"
 
 client = Client.from_client_credentials(client_id, client_secret, redirect_url)
 
-ID = 509388
-beatmap_scores = client.get_beatmap_scores(ID)
-print("Top 5 scores on this beatmap (Standard):")
-print("\n".join([f'{i+1}. {score.user.username} {"+"+score.mods.to_readable_string() if score.mods is not None else ""} - '
-                 f'{score.pp}' for i, score in enumerate(beatmap_scores.scores[:5])]))
+
+def readable_mod(mods):
+    if not mods:
+        return ""
+    if isinstance(mods, Mods):
+        return "+"+mods.to_readable_string()
+    return "+"+"".join(map(lambda x: x.mod.value, mods))
 
 
-beatmap_scores = client.get_beatmap_scores(ID, GameModeStr.TAIKO)
-print("Top 5 scores on this beatmap (Taiko):")
-print("\n".join([f'{i+1}. {score.user.username} {"+"+score.mods.to_readable_string() if score.mods is not None else ""} - '
-                 f'{score.pp}' for i, score in enumerate(beatmap_scores.scores[:5])]))
+def do(mode, lazer=False):
+    beatmap_scores = (client.get_beatmap_scores if not lazer else client.get_lazer_beatmap_scores)(878369, mode)
+    print(f"Top 5 scores on this beatmap ({mode.name}, lazer={lazer}):")
+    print("\n".join(
+        [f'{i + 1}. {score.user.username} {readable_mod(score.mods)} - '
+         f'{score.pp}' for i, score in enumerate(beatmap_scores.scores[:5])]))
+    print()
+
+
+do(GameModeStr.STANDARD)
+do(GameModeStr.TAIKO)
+do(GameModeStr.STANDARD, True)
