7005e8c2c31f58fc6d1abb18b82ba6cc516b44ca
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2023-01-23 22:28:27 -0600

    rename variable

--- examples/get_user_beatmap_scores.py
+++ examples/get_user_beatmap_scores.py
@@ -8,8 +8,8 @@ redirect_url = "http://127.0.0.1:8080"
 
 client = Client.from_client_credentials(client_id, client_secret, redirect_url)
 
-ID = 2335553
+beatmap_id = 2335553
 user_id = 14895608
-beatmap_scores = client.get_user_beatmap_scores(ID, user_id)
+beatmap_scores = client.get_user_beatmap_scores(beatmap_id, user_id)
 for score in beatmap_scores:
     print(f"{score.mods} {score.accuracy} {score.pp}")
