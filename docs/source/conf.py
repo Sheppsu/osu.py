f5177a3c2ce851ce577672d1ccf65bcf89730f25
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2023-03-10 22:45:36 -0600

    v0.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Sheepposu'
 author = 'Sheepposu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
--- osu/__init__.py
+++ osu/__init__.py
@@ -13,4 +13,4 @@ from .util import (
 )
 
 
-__version__ = '0.5.1'
+__version__ = '0.6.0'
