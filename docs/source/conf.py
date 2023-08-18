863adda489f20e28eeefe8b8909689eee693dcec
Sheepposu <apc2005osu@gmail.com>
2023-08-17 19:59:02 -0500

    v1.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Sheppsu'
 author = 'Sheepposu'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
