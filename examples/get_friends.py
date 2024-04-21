928d8757b6564955f4f844d0d88ec034d1e511db
Sheepposu <apc2005osu@gmail.com>
2024-04-21 00:17:30 -0400

    refactor example code

--- examples/get_friends.py
+++ examples/get_friends.py
@@ -2,10 +2,13 @@ from osu import Client, AuthHandler, Scope
 import os
 
 
-auth = AuthHandler(int(os.getenv('osu_client_id')), os.getenv('osu_client_secret'),
-                   os.getenv('osu_redirect_uri'), Scope('friends.read'))
-print(auth.get_auth_url())
-auth.get_auth_token(input("Code: "))
+auth = AuthHandler(
+    int(os.getenv('CLIENT_ID')),
+    os.getenv('CLIENT_SECRET'),
+    os.getenv('REDIRECT_URI'),
+    Scope("public", "identify", "friends.read")
+)
+auth.get_auth_token(input(f"{auth.get_auth_url()}\nCode: "))
 client = Client(auth)
 
 print(client.get_friends())
--- examples/util_for_examples.py
+++ examples/util_for_examples.py
@@ -1,12 +1,16 @@
 import json
 import os
-from osu import Client, AuthHandler, Scope, AsynchronousClient
+from osu import Client, AuthHandler, Scope
 
 
 def get_user_client():
     def save(auth):
         with open("auth.json", "w") as f:
             json.dump(auth.get_save_data(), f)
+
+    if not os.path.exists("auth.json"):
+        with open("auth.json", "w") as f:
+            f.write("{}")
 
     with open("auth.json", "r") as f:
         auth_data = json.load(f)
@@ -16,13 +20,12 @@ def get_user_client():
         client = Client(AuthHandler.from_save_data(auth_data))
     else:
         print("No auth data found. Creating auth from scratch.")
-        client_id = int(os.getenv('osu_client_id'))
-        client_secret = os.getenv('osu_client_secret')
+        client_id = int(os.getenv('CLIENT_ID'))
+        client_secret = os.getenv('CLIENT_SECRET')
         redirect_url = "http://127.0.0.1:8080"
 
         auth = AuthHandler(client_id, client_secret, redirect_url, Scope.identify())
-        print(auth.get_auth_url())
-        auth.get_auth_token(input("Code: "))
+        auth.get_auth_token(input(f"Get the code from authorizing with this url\n{auth.get_auth_url()}\nCode: "))
         client = Client(auth)
 
     save(client.auth)
