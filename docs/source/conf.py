0a2338763d09da1b3f56b4f171bd98e3e6a6455e
Sheepposu <apc2005osu@gmail.com>
2024-04-21 04:53:28 -0400

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2024, Sheppsu'
 author = 'Sheepposu'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
--- osu/__init__.py
+++ osu/__init__.py
@@ -14,4 +14,4 @@ from .util import (
 from .results import *
 
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
