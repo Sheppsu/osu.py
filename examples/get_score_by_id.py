a448d80911164d71a35ca8550123a1b4b6ad0b8a
Sheppsu <apc2005osu@gmail.com>
2024-05-31 10:18:28 -0500

    Create get_score_by_id.py

---
+++ examples/get_score_by_id.py
@@ -0,0 +1,7 @@
+from osu import Client
+import os
+
+
+client = Client.from_client_credentials(int(os.getenv("CLIENT_ID")), os.getenv("CLIENT_SECRET"), None)
+score = client.get_score_by_id_only(2843952435)
+print(score)
