0fb04e0a18843d3c540f91fc9a3d40b6ba424bc8
Sheppsu <apc2005osu@gmail.com>
2024-08-06 13:06:33 -0500

    v2.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2024, Sheppsu'
 author = 'Sheppsu'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.0'
+release = '2.2.1'
 
 
 # -- General configuration ---------------------------------------------------
