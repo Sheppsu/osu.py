396404fc8d6430eb96bdca992d7e79818e745dea
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-10-23 03:19:28 -0400

    update docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2024, Sheppsu'
 author = 'Sheppsu'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.2'
+release = '3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
