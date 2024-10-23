ca051575d503c5fd8efbc2ef57c004ff6973112f
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-10-23 02:50:40 -0400

    move to parent dir

---
+++ auth.py
@@ -0,0 +1,34 @@
+import os
+import json
+from osu import AuthHandler, Scope
+
+
+CLIENT_ID = int(os.getenv("CLIENT_ID"))
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+DEV_CLIENT_ID = int(os.getenv("DEV_CLIENT_ID"))
+DEV_CLIENT_SECRET = os.getenv("DEV_CLIENT_SECRET")
+DEV_REDIRECT_URI = os.getenv("DEV_REDIRECT_URI")
+
+
+auth = AuthHandler(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+    Scope("public", "identify", "friends.read"),
+)
+auth.get_auth_token(input(f"{auth.get_auth_url()}\nCode: "))
+with open("tests/auth.json", "w") as f:
+    json.dump(auth.get_save_data(), f)
+
+auth = AuthHandler(
+    DEV_CLIENT_ID,
+    DEV_CLIENT_SECRET,
+    DEV_REDIRECT_URI,
+    Scope("public", "identify", "friends.read", "forum.write", "chat.read", "chat.write", "chat.write_manage")
+)
+auth.set_domain("dev.ppy.sh")
+auth.get_auth_token(input(f"{auth.get_auth_url()}\nCode: "))
+with open("tests/dev-auth.json", "w") as f:
+    json.dump(auth.get_save_data(), f)
