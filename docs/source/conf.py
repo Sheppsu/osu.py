4132fdbf614a7344b815106741ae61249a0723dd
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-12-10 01:41:50 -0500

    v3.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2024, Sheppsu'
 author = 'Sheppsu'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.0'
+release = '3.2.0'
 
 
 # -- General configuration ---------------------------------------------------
--- osu/__init__.py
+++ osu/__init__.py
@@ -9,4 +9,4 @@ from .util import *
 from .results import *
 
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
